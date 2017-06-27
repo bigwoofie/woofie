610f22f710f0514a92964050cef3be527988afbe
Louis Roehrs <lroehrs@us.ibm.com>
2017-06-26 22:51:39 -0700

    updated for browser evolution

--- server.js
+++ server.js
@@ -7,133 +7,133 @@ var starttime = (new Date()).getTime();
 
 var mem = process.memoryUsage();
 // every 10 seconds poll for the memory.
-  setInterval(function () {
-  mem = process.memoryUsage();
+setInterval(function () {
+    mem = process.memoryUsage();
 }, 10*1000);
 
 
 var fu = require("./js/fu"),
-    sys = require("sys"),
-    url = require("url"),
-    qs = require("querystring");
+sys = require("sys"),
+url = require("url"),
+qs = require("querystring");
 
 var MESSAGE_BACKLOG = 2000000000000000000000,
-    SESSION_TIMEOUT = 60000 * 1000;
+SESSION_TIMEOUT = 60000 * 1000;
 
 var channel = new function () {
-  var messages = [],
-      callbacks = [];
-
-				//  this.appendMessage = function (nick, type, text) {
-
-  this.appendMessage = function (nick, message) {
-
-    switch (message.type) {
-      case "msg":
-        sys.puts("<" + nick + "> " + message.text);
-        break;
-      case "join":
-        sys.puts(nick + " join");
-        break;
-      case "part":
-        sys.puts(nick + " part");
-        break;
-
-      case "newBlock":
-					// Server sets the GUID for new things
-			  message.guid = newGuid();
-  			sys.puts( nick + ": " + JSON.stringify(message));
-        break;
-
-		default:
-			//			sys.puts(nick+ ": " + JSON.stringify(message));
-    }
-
-
-    messages.push( message );
-
-    while (callbacks.length > 0) {
-      callbacks.shift().callback([message]);
-    }
-
-    while (messages.length > MESSAGE_BACKLOG) {
-      messages.shift();
-		}
-  };
-
-  this.query = function (since, callback) {
-    var matching = [];
-    for (var i = 0; i < messages.length; i++) {
-      var message = messages[i];
-      if (message.timestamp > since)
-        matching.push(message)
-    }
-
-    if (matching.length != 0) {
-      callback(matching);
-    } else {
-      callbacks.push({ timestamp: new Date(), callback: callback });
-    }
-  };
-
-  // clear old callbacks
-  // they can hang around for at most 30 seconds.
-  setInterval(function () {
-    var now = new Date();
-    while (callbacks.length > 0 && now - callbacks[0].timestamp > 30*1000) {
-      callbacks.shift().callback([]);
-    }
-  }, 3000);
+    var messages = [],
+    callbacks = [];
+    
+    //  this.appendMessage = function (nick, type, text) {
+    
+    this.appendMessage = function (nick, message) {
+        
+        switch (message.type) {
+        case "msg":
+            sys.puts("<" + nick + "> " + message.text);
+            break;
+        case "join":
+            sys.puts(nick + " join");
+            break;
+        case "part":
+            sys.puts(nick + " part");
+            break;
+            
+        case "newBlock":
+            // Server sets the GUID for new things
+            message.guid = newGuid();
+            sys.puts( nick + ": " + JSON.stringify(message));
+            break;
+            
+        default:
+            //            sys.puts(nick+ ": " + JSON.stringify(message));
+        }
+        
+        
+        messages.push( message );
+        
+        while (callbacks.length > 0) {
+            callbacks.shift().callback([message]);
+        }
+        
+        while (messages.length > MESSAGE_BACKLOG) {
+            messages.shift();
+        }
+    };
+    
+    this.query = function (since, callback) {
+        var matching = [];
+        for (var i = 0; i < messages.length; i++) {
+            var message = messages[i];
+            if (message.timestamp > since)
+                matching.push(message)
+        }
+        
+        if (matching.length != 0) {
+            callback(matching);
+        } else {
+            callbacks.push({ timestamp: new Date(), callback: callback });
+        }
+    };
+    
+    // clear old callbacks
+    // they can hang around for at most 30 seconds.
+    setInterval(function () {
+        var now = new Date();
+        while (callbacks.length > 0 && now - callbacks[0].timestamp > 30*1000) {
+            callbacks.shift().callback([]);
+        }
+    }, 3000);
 };
 
 var sessions = {};
 var GUID = 0;
 
 function newGuid() {
-		GUID++;
-		//		return new Date().toString()+GUID;
-		return GUID;
+    GUID++;
+    //        return new Date().toString()+GUID;
+    return GUID;
 }
 
 function createSession (nick) {
-  if (nick.length > 50) return null;
-  if (/[^\w_\-^!]/.exec(nick)) return null;
-
-  for (var i in sessions) {
-    var session = sessions[i];
-    if (session && session.nick === nick) return null;
-  }
-
-  var session = { 
-    nick: nick, 
-    id: Math.floor(Math.random()*99999999999).toString(),
-    timestamp: new Date(),
-
-    poke: function () {
-      session.timestamp = new Date();
-    },
-
-    destroy: function () {
-			channel.appendMessage(session.nick, {type:"part",text:"part"});
-			delete sessions[session.id];
+    if (nick.length > 50) return null;
+    if (/[^\w_\-^!]/.exec(nick)) return null;
+    
+    for (var i in sessions) {
+        var session = sessions[i];
+        if (session && session.nick === nick) return null;
     }
-  };
-
-  sessions[session.id] = session;
-  return session;
+    
+    var session = { 
+        nick: nick, 
+        id: Math.floor(Math.random()*99999999999).toString(),
+        timestamp: new Date(),
+        
+        poke: function () {
+            session.timestamp = new Date();
+        },
+        
+        destroy: function () {
+            channel.appendMessage(session.nick, {type:"part",text:"part"});
+            delete sessions[session.id];
+        }
+    };
+    
+    sessions[session.id] = session;
+    return session;
 }
 
 // interval to kill off old sessions
 setInterval(function () {
-  var now = new Date();
-  for (var id in sessions) {
-    if (!sessions.hasOwnProperty(id)) continue;
-    var session = sessions[id];
-
-    if (now - session.timestamp > SESSION_TIMEOUT) {
-      session.destroy();
+    var now = new Date();
+    for (var id in sessions) {
+        if (!sessions.hasOwnProperty(id)) continue;
+        var session = sessions[id];
+        
+        if (now - session.timestamp > SESSION_TIMEOUT) {
+            session.destroy();
+        }
     }
-  }
 }, 1000);
 
 fu.listen(Number(process.env.PORT || PORT), HOST);
@@ -198,99 +198,100 @@ fu.get("/sprites/lucy.png", fu.staticHandler("sprites/lucy.png"));
 fu.get("/sprites/sophia.png", fu.staticHandler("sprites/sophia.png"));
 
 fu.get("/who", function (req, res) {
-  var nicks = [];
-  for (var id in sessions) {
-    if (!sessions.hasOwnProperty(id)) continue;
-    var session = sessions[id];
-    nicks.push(session.nick);
-  }
-  res.simpleJSON(200, { nicks: nicks
-                      , rss: mem.rss
-                      });
+    var nicks = [];
+    for (var id in sessions) {
+        if (!sessions.hasOwnProperty(id)) continue;
+        var session = sessions[id];
+        nicks.push(session.nick);
+    }
+    res.simpleJSON(200, { nicks: nicks
+                          , rss: mem.rss
+                        });
 });
 
 fu.get("/join", function (req, res) {
-	var message = JSON.parse(qs.parse(url.parse(req.url).query).message);
-	nick = message.nick;
-  if (nick == null || nick.length == 0) {
-    res.simpleJSON(400, {error: "Bad nick."});
-    return;
-  }
-  var session = createSession(nick);
-  if (session == null) {
-    res.simpleJSON(400, {error: "Nick in use"});
-    return;
-  }
-
-  //sys.puts("connection: " + nick + "@" + res.connection.remoteAddress);
-
-  channel.appendMessage(session.nick, message);
-  res.simpleJSON(200, { id: session.id
-							, message: JSON.stringify( {nick: session.nick, 
-													                type: "join", 
-													                text:"join",
-													timestamp: new Date().getTime()
-													}) 
-                      , rss: mem.rss
-                      , starttime: starttime
-                      });
+    var message = JSON.parse(qs.parse(url.parse(req.url).query).message);
+    nick = message.nick;
+    if (nick == null || nick.length == 0) {
+        res.simpleJSON(400, {error: "Bad nick."});
+        return;
+    }
+    var session = createSession(nick);
+    if (session == null) {
+        res.simpleJSON(400, {error: "Nick in use"});
+        return;
+    }
+    
+    //sys.puts("connection: " + nick + "@" + res.connection.remoteAddress);
+    
+    channel.appendMessage(session.nick, message);
+    res.simpleJSON(200, { id: session.id
+                          , message: JSON.stringify( {nick: session.nick, 
+                                                      type: "join", 
+                                                      text:"join",
+                                                      timestamp: new Date().getTime()
+                                                     }) 
+                          , rss: mem.rss
+                          , starttime: starttime
+                        });
 });
 
 fu.get("/part", function (req, res) {
-  var id = qs.parse(url.parse(req.url).query).id;
-  var session;
-  if (id && sessions[id]) {
-    session = sessions[id];
-    session.destroy();
-  }
-  res.simpleJSON(200, { rss: mem.rss });
+    var id = qs.parse(url.parse(req.url).query).id;
+    var session;
+    if (id && sessions[id]) {
+        session = sessions[id];
+        session.destroy();
+    }
+    res.simpleJSON(200, { rss: mem.rss });
 });
 
 fu.get("/recv", function (req, res) {
-  if (!qs.parse(url.parse(req.url).query).since) {
-    res.simpleJSON(400, { error: "Must supply since parameter" });
-    return;
-  }
-  var id = qs.parse(url.parse(req.url).query).id;
-  var session;
-  if (id && sessions[id]) {
-    session = sessions[id];
-    session.poke();
-  }
-
-  var since = parseInt(qs.parse(url.parse(req.url).query).since, 10);
-
-  channel.query(since, function (messages) {
-    if (session) session.poke();
-    res.simpleJSON(200, { messages: messages, rss: mem.rss });
-  });
+    if (!qs.parse(url.parse(req.url).query).since) {
+        res.simpleJSON(400, { error: "Must supply since parameter" });
+        return;
+    }
+    var id = qs.parse(url.parse(req.url).query).id;
+    var session;
+    if (id && sessions[id]) {
+        session = sessions[id];
+        session.poke();
+    }
+    
+    var since = parseInt(qs.parse(url.parse(req.url).query).since, 10);
+    
+    channel.query(since, function (messages) {
+        if (session) session.poke();
+        res.simpleJSON(200, { messages: messages, rss: mem.rss });
+    });
 });
 
 
 sendResponse = function (req, res) {
 
-  var id = qs.parse(url.parse(req.url).query).id;
-  var message = JSON.parse(qs.parse(url.parse(req.url).query).message);
-
-  var session = sessions[id];
-  if (!session || !message.type) {
-    res.simpleJSON(400, { error: "No such session id" });
-    return;
-  }
-
-  session.poke();
-
-	message.nick = session.nick;
-	message.timestamp = new Date().getTime();
-  channel.appendMessage(session.nick, message);
-  res.simpleJSON(200, { rss: mem.rss });
+    console.log(req);
+    var id = qs.parse(url.parse(req.url).query).id;
+    var message = JSON.parse(qs.parse(url.parse(req.url).query).message);
+    
+    var session = sessions[id];
+    if (!session || !message.type) {
+        res.simpleJSON(400, { error: "No such session id" });
+        return;
+    }
+    
+    session.poke();
+    
+    message.nick = session.nick;
+    message.timestamp = new Date().getTime();
+    channel.appendMessage(session.nick, message);
+    res.simpleJSON(200, { rss: mem.rss });
 };
 
 
 fu.get("/send", function (req,res) {
-				sendResponse(req,res);
-		});
-		
+    sendResponse(req,res);
+});
+
 
 /* need to make a new /move and simpleJSON to move location data.
-	 Do we need to store it? */
+  Do we need to store it? */
